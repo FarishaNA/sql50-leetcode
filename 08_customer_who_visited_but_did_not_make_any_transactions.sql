623be2d60e57c711c45c2d108bf8517bf68499bb
Farisha N A <147705381+FarishaNA@users.noreply.github.com>
2026-03-07 15:34:19 +0530

    problem 8

---
+++ 08_customer_who_visited_but_did_not_make_any_transactions.sql
@@ -0,0 +1,9 @@
+-- Customer Who Visited but Did Not Make Any Transactions
+-- https://leetcode.com/problems/customer-who-visited-but-did-not-make-any-transactions/
+
+SELECT customer_id, COUNT(*) AS count_no_trans
+FROM Visits
+WHERE visit_id NOT IN (
+    SELECT visit_id FROM Transactions
+)
+GROUP BY customer_id;
