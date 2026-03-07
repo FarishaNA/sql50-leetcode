dc74a5ce37452899455ce3c49745000d0867009b
Farisha N A <147705381+FarishaNA@users.noreply.github.com>
2026-03-07 15:17:36 +0530

    problem 6

---
+++ 06_replace_employee_id_with_the_unique_identifier.sql
@@ -0,0 +1,7 @@
+-- Replace Employee ID With The Unique Identifier
+-- https://leetcode.com/problems/replace-employee-id-with-the-unique-identifier/
+
+SELECT u.unique_id, e.name
+FROM Employees e
+LEFT JOIN EmployeeUNI u
+ON e.id = u.id;
