4f966e3901017476519651e01054b1ec95c87a84
Farisha N A <147705381+FarishaNA@users.noreply.github.com>
2026-03-11 00:28:24 +0530

    problem 9

---
+++ 09_rising_temperature.sql
@@ -0,0 +1,10 @@
+-- Rising Temperature
+-- https://leetcode.com/problems/rising-temperature/
+
+SELECT w1.id
+FROM Weather w1
+WHERE w1.temperature > (
+    SELECT w2.temperature
+    FROM Weather w2
+    WHERE w2.recordDate = DATE_SUB(w1.recordDate, INTERVAL 1 DAY)
+);
