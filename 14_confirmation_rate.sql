229841de41588473dbbe77021728a81d32a2d74b
Farisha N A <147705381+FarishaNA@users.noreply.github.com>
2026-03-16 23:53:54 +0530

    Problem 14

---
+++ 14_confirmation_rate.sql
@@ -0,0 +1,9 @@
+-- Confirmation Rate
+-- https://leetcode.com/problems/confirmation-rate/
+
+SELECT s.user_id,
+ROUND(IFNULL(SUM(c.action = 'confirmed') / COUNT(*), 0), 2) AS confirmation_rate
+FROM Signups s
+LEFT JOIN Confirmations c
+ON s.user_id = c.user_id
+GROUP BY s.user_id;
