808c504da182548ef2a26ea81555e338dcccf73b
Farisha N A <147705381+FarishaNA@users.noreply.github.com>
2026-03-16 06:41:08 +0530

    Problem 13

---
+++ 13_managers_with_at_least_5_direct_reports.sql
@@ -0,0 +1,11 @@
+-- Managers with at Least 5 Direct Reports
+-- https://leetcode.com/problems/managers-with-at-least-5-direct-reports/
+
+SELECT name
+FROM Employee
+WHERE id IN (
+    SELECT managerId
+    FROM Employee
+    GROUP BY managerId
+    HAVING COUNT(*) >= 5
+);
