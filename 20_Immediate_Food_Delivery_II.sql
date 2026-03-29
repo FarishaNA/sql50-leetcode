6656b5ec947de837dde0a8939a6f7f0acfd4235e
Farisha N A <147705381+FarishaNA@users.noreply.github.com>
2026-03-29 22:04:53 +0530

    Problem 20

---
+++ 20_Immediate_Food_Delivery_II.sql
@@ -0,0 +1,13 @@
+-- Immediate Food Delivery II
+-- https://leetcode.com/problems/immediate-food-delivery-ii/
+
+SELECT 
+    ROUND(100.0 * SUM(d.customer_pref_delivery_date = d.order_date) / COUNT(*), 2) AS immediate_percentage
+FROM Delivery d
+JOIN (
+    SELECT customer_id, MIN(order_date) AS first_order
+    FROM Delivery
+    GROUP BY customer_id
+) f
+ON d.customer_id = f.customer_id
+AND d.order_date = f.first_order;
