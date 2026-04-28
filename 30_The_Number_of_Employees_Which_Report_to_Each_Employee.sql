fe27f4e6bc51d9ff08ca94d8861ee902a75944ab
Farisha N A <147705381+FarishaNA@users.noreply.github.com>
2026-04-28 10:30:17 +0530

    Problem 30

---
+++ 30_The_Number_of_Employees_Which_Report_to_Each_Employee.sql
@@ -0,0 +1,13 @@
+-- The Number of Employees Which Report to Each Employee
+-- https://leetcode.com/problems/the-number-of-employees-which-report-to-each-employee/
+
+SELECT 
+    mgr.employee_id,
+    mgr.name,
+    COUNT(emp.employee_id) AS reports_count,
+    ROUND(AVG(emp.age)) AS average_age
+FROM Employees emp
+JOIN Employees mgr
+ON emp.reports_to = mgr.employee_id
+GROUP BY mgr.employee_id, mgr.name
+ORDER BY mgr.employee_id;
