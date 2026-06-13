2c9069f8a4431819a056dcbb1c1dfea162f170d3
Farisha N A <147705381+FarishaNA@users.noreply.github.com>
2026-06-13 21:14:16 +0530

    Problem 49

---
+++ 49_List_the_Products_Ordered_in_a_Period.sql
@@ -0,0 +1,12 @@
+-- List the Products Ordered in a Period
+-- https://leetcode.com/problems/list-the-products-ordered-in-a-period/
+
+SELECT
+    p.product_name,
+    SUM(o.unit) AS unit
+FROM Products p
+JOIN Orders o
+    ON p.product_id = o.product_id
+WHERE o.order_date BETWEEN '2020-02-01' AND '2020-02-29'
+GROUP BY p.product_id
+HAVING SUM(o.unit) >= 100;
