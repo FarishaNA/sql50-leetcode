19a16a2bdb9fd1a4683947b26c3c7b2ff3301c09
Farisha N A <147705381+FarishaNA@users.noreply.github.com>
2026-06-16 18:01:28 +0530

    Problem 41

---
+++ 41_Friend_Requests_II_Who_Has_the_Most_Friends.sql
@@ -0,0 +1,18 @@
+-- Friend Requests II: Who Has the Most Friends
+-- https://leetcode.com/problems/friend-requests-ii-who-has-the-most-friends/
+
+SELECT
+    id,
+    COUNT(*) AS num
+FROM (
+    SELECT requester_id AS id
+    FROM RequestAccepted
+
+    UNION ALL
+
+    SELECT accepter_id AS id
+    FROM RequestAccepted
+) c
+GROUP BY id
+ORDER BY num DESC
+LIMIT 1;
