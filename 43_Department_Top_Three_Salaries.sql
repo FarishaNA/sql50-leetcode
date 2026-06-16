9abd1c230152b3f2b49e2f96f23c5e1845213796
Farisha N A <147705381+FarishaNA@users.noreply.github.com>
2026-06-16 23:54:17 +0530

    Problem 43

---
+++ 43_Department_Top_Three_Salaries.sql
@@ -0,0 +1,18 @@
+-- Department Top Three Salaries
+-- https://leetcode.com/problems/department-top-three-salaries/
+
+SELECT
+    d.name AS Department,
+    e.name AS Employee,
+    e.salary AS Salary
+FROM (
+    SELECT *,
+           DENSE_RANK() OVER (
+               PARTITION BY departmentId
+               ORDER BY salary DESC
+           ) AS rnk
+    FROM Employee
+) e
+JOIN Department d
+    ON e.departmentId = d.id
+WHERE e.rnk <= 3;
